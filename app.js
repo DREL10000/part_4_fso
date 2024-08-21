5f55023bfd4586491aed4ecd8904be7ff0a19f98
DREL10000 <variabledev1@gmail.com>
2024-08-21 19:03:25 +0200

    Refactored project into modules. completed exercise 4.2

---
+++ app.js
@@ -0,0 +1,14 @@
+const Blog = require('./models/blog')
+const logger = require('./utils/logger')
+const config = require('./utils/config')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
