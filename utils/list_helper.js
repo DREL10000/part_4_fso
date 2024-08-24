48f5ba2ef93e5d2c243a90cea914d35aaed397b9
DREL10000 <variabledev1@gmail.com>
2024-08-24 12:37:29 +0200

    completed exercises up until exercise 4.7

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -1,3 +1,5 @@
+let _ = require('lodash');
+
 const dummy = (blogs) => {
     return 1;
 }
@@ -9,7 +11,37 @@ const totalLikes = (blogs) => {
     return t
 }
 
+const favoriteBlog = (blogs) => {
+    let max = Math.max(...blogs.map((b) => b.likes))
+    return blogs.find((b) => b.likes === max)
+}
+
+const mostBlogs = (blogs) => {
+    const obj = _.countBy(blogs, 'author')
+    //{name: 1, name: 2, name: 3}
+    const sortedAuthorCounts = _.sortBy(obj, (v) => -v)
+    const auth = Object.entries(obj).find((e) => e[1] === sortedAuthorCounts[0])
+    const res = { author: auth[0], blogs: auth[1] }
+    return res
+}
+
+const mostLikes = (blogs) => {
+    const names = _.uniq(blogs.map((obj) => obj.author))
+    let winner = { author: '', likes: 0 }
+    names.forEach((name) => {
+        let filtered = blogs.filter((b) => b.author === name)
+        let total = filtered.reduce((sum, i) => sum += i.likes, 0)
+        if (total > winner.likes) {
+            winner = { author: name, likes: total }
+        }
+    })
+    return winner
+}
+
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
 }
